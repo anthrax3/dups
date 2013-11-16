2bcb6d9e42384b4e06cae10ae91a109b16c4f093
Graham Brooks <graham@grahambrooks.com>
2013-11-16 18:30:41 -0500

    Broke out the bucket code into its own file so it can be used

--- test/bucket_tests.cpp
+++ test/bucket_tests.cpp
@@ -1,34 +1,6 @@
 #include <boost/test/unit_test.hpp>
-#include <string>
-#include <map>
-#include <vector>
 
-namespace duplicate {
-
-  class match {
-  };
-
-  class bucket {
-    std::map<std::string, std::vector<match> > contents;
-  public:
-    void add(std::string key, match m) {
-
-      if (contents.find(key) == contents.end()) {
-	std::vector<match> a;
-	contents[key] = a;
-      }
-      contents[key].push_back(m);
-    }
-
-    std::vector<match>& operator[](const std::string& key) {
-      return contents[key];
-    }
-
-    bool is_empty() {
-      return contents.size() == 0;
-    }
-  };
-}
+#include "bucket.hpp"
 
 
 BOOST_AUTO_TEST_CASE(buckets_start_empty) {
