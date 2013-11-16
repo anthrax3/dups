fa97cd9966d479f6540f346181b47c4b9567d6d7
Graham Brooks <graham@grahambrooks.com>
2013-11-16 18:05:01 -0500

    Buckets start out empty

--- test/bucket_tests.cpp
+++ test/bucket_tests.cpp
@@ -1,6 +1,18 @@
 #include <boost/test/unit_test.hpp>
 
+namespace duplicatate {
+  class bucket {
+    public:
+    bool is_empty() {
+      return true;
+    }
+  };
+}
+
+
 BOOST_AUTO_TEST_CASE(buckets_start_empty) {
   
-  BOOST_CHECK(true);
+  duplicatate::bucket bucket;
+
+  BOOST_CHECK(bucket.is_empty());
 }
