9bab989ec450056b453b27e827e4f3fd94069889
Graham Brooks <graham@grahambrooks.com>
2013-11-16 18:22:07 -0500

    Can add to buckets

--- test/bucket_tests.cpp
+++ test/bucket_tests.cpp
@@ -1,10 +1,26 @@
 #include <boost/test/unit_test.hpp>
+#include <string>
+#include <map>
+#include <vector>
 
 namespace duplicate {
+
+  class match {
+  };
+
   class bucket {
-    public:
+    std::map<std::string, std::vector<match> > contents;
+  public:
+    void add(std::string key, match m) {
+
+      if (contents.find(key) == contents.end()) {
+	std::vector<match> a;
+	contents[key] = a;
+      }
+      contents[key].push_back(m);
+    }
     bool is_empty() {
-      return true;
+      return contents.size() == 0;
     }
   };
 }
