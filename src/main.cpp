f45e36367d159619e70aeb8636c7e2e871795417
Graham Brooks <graham@grahambrooks.com>
2013-11-17 17:00:59 -0500

    added accepting a path on the command line

--- src/main.cpp
+++ src/main.cpp
@@ -7,28 +7,31 @@ int main(int argc, const char *argv[]) {
   cout << "dups - duplicate text finder" << std::endl;
 
   duplicate::duplicate_text_finder finder(6);
+  
+  if (argc > 1) {
 
-  auto duplicates = finder.find(".");
-
-  for (auto dup : duplicates) {
-    std::cout << dup.second[0].range().size() << " duplicate lines found" << std::endl;
-
-    for (auto m : dup.second) {
-      std::cout << "  " << m.path() << " " << m.range() << std::endl;
-      std::cout << "----------------------" << std::endl;
+    auto duplicates = finder.find(argv[1]);
+    
+    for (auto dup : duplicates) {
+      std::cout << dup.second[0].range().size() << " duplicate lines found" << std::endl;
       
-      ifstream p(m.path().string());
-
-      auto line_number = 0;
-
-      for (std::string line; std::getline(p, line);) {
-	line_number++;
-
-	if (m.range().contains(line_number)) {
-	  std::cout << line << std::endl;
+      for (auto m : dup.second) {
+	std::cout << "  " << m.path() << " " << m.range() << std::endl;
+	std::cout << "----------------------" << std::endl;
+	
+	ifstream p(m.path().string());
+	
+	auto line_number = 0;
+	
+	for (std::string line; std::getline(p, line);) {
+	  line_number++;
+	  
+	  if (m.range().contains(line_number)) {
+	    std::cout << line << std::endl;
+	  }
 	}
+	std::cout << "----------------------" << std::endl;
       }
-      std::cout << "----------------------" << std::endl;
     }
   }
 }
